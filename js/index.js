757367801295e5d9c0fa5edf0f985e259dfb0238
Acry <zvar.vt@gmail.com>
2021-11-03 18:42:27 +0200

    Cart modal added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 const modal = document.querySelector(".modal");
 const openModal = document.querySelector(".modal__open");
 const closeModal = document.querySelector(".modal__close");
+const cartButton = document.querySelector("#cart-btn");
+const cartModal = document.querySelector(".cart");
+
 
 openModal.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 
 function toggleModal() {
     modal.classList.toggle("modalshow");
+}
+
+cartButton.addEventListener("click", cartToggle);
+
+function cartToggle() {
+    cartModal.classList.toggle("cartshow");
 }
